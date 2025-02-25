512cd931d7ece522fa15240ea94b922bab123fb2
Mohtasim Hadi Rafi <mzr0167@auburn.edu>
2025-02-25 10:53:22 -0600

    forms updated

--- analysis/forms.py
+++ analysis/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.forms.widgets import ClearableFileInput
+
+class MultiFileInput(ClearableFileInput):
+    allow_multiple_selected = True
 
 class ImageUploadForm(forms.Form):
-    images = forms.ImageField(
-        widget=forms.ClearableFileInput(attrs={'multiple': True})
-    )
+    images = forms.ImageField(widget=MultiFileInput(attrs={'multiple': True}))
